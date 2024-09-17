4b7b18353c83dd43be8794d9377d744163290aa6
Christian Castaneda <crhisjairo@hotmail.com>
2024-09-16 22:45:13 -0400

    Update script-db-v01.sql

--- backend/db-diagrams/script-db-v01.sql
+++ backend/db-diagrams/script-db-v01.sql
@@ -23,7 +23,7 @@ USE `inm5151_db` ;
 DROP TABLE IF EXISTS `inm5151_db`.`type` ;
 
 CREATE TABLE IF NOT EXISTS `inm5151_db`.`type` (
-  `id_type` INT NOT NULL DEFAULT 0,
+  `id_type` INT NOT NULL AUTO_INCREMENT,
   `name_fr` VARCHAR(100) NULL,
   `name_en` VARCHAR(100) NULL,
   `name_la` VARCHAR(100) NULL,
@@ -147,9 +147,9 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `inm5151_db`;
-INSERT INTO `inm5151_db`.`type` (`id_type`, `name_fr`, `name_en`, `name_la`) VALUES (0, 'Catalpa de l\'Ouest', 'Western Catalpa', 'Catalpa speciosa');
-INSERT INTO `inm5151_db`.`type` (`id_type`, `name_fr`, `name_en`, `name_la`) VALUES (1, 'Phellodendron de l\'Amour Maelio', 'Maelio Amur Cork Tree', 'Phellodendron amurensis \'Maelio\'');
-INSERT INTO `inm5151_db`.`type` (`id_type`, `name_fr`, `name_en`, `name_la`) VALUES (2, 'Érable à Giguère', 'Manitoba Maple', 'Acer negundo');
+INSERT INTO `inm5151_db`.`type` (`name_fr`, `name_en`, `name_la`) VALUES ('Catalpa de lOuest', 'Western Catalpa', 'Catalpa speciosa');
+INSERT INTO `inm5151_db`.`type` (`name_fr`, `name_en`, `name_la`) VALUES ('Phellodendron de lAmour Maelio', 'Maelio Amur Cork Tree', 'Phellodendron amurensis Maelio');
+INSERT INTO `inm5151_db`.`type` (`name_fr`, `name_en`, `name_la`) VALUES ('Érable à Giguère', 'Manitoba Maple', 'Acer negundo');
 
 COMMIT;
 
@@ -159,16 +159,16 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `inm5151_db`;
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (0, '1A', 'Conifères généralement tolérants à l’ombre, mais pas à la sécheresse ou l’inondation.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (1, '1B', 'Conifères héliophiles (qui aime la lumière), tolérants à la sécheresse.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (2, '2A', 'Arbres tolérants à l’ombre à feuilles larges et minces, croissance moyenne. Graines dispersées surtout par le vent.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (3, '2B', 'Ressemblent au 2A sauf pour les semences très lourdes et dispersées par gravité.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (4, '2C', 'Grands arbres tolérants à l’inondation.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (5, '3A', 'Petits arbres tolérants à la sécheresse, bois lourd, feuilles épaisses, croissance faible.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (6, '3B', 'Groupe « moyen ». Intolérant à l’inondation.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (7, '4A', 'Grands arbres à semences et bois lourds. Plusieurs tolérants à la sécheresse.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (8, '4B', 'Grande tolérance à la sécheresse, mais pas à l’ombre ou inondation.');
-INSERT INTO `inm5151_db`.`functional_group` (`id_functional_group`, `group`, `description`) VALUES (9, '5', 'Espèces pionnières à très petites semences. Croissance rapide, tolérants à l’inondation, bois léger.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('1A', 'Conifères généralement tolérants à l’ombre, mais pas à la sécheresse ou l’inondation.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('1B', 'Conifères héliophiles (qui aime la lumière), tolérants à la sécheresse.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('2A', 'Arbres tolérants à l’ombre à feuilles larges et minces, croissance moyenne. Graines dispersées surtout par le vent.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('2B', 'Ressemblent au 2A sauf pour les semences très lourdes et dispersées par gravité.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('2C', 'Grands arbres tolérants à l’inondation.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('3A', 'Petits arbres tolérants à la sécheresse, bois lourd, feuilles épaisses, croissance faible.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('3B', 'Groupe « moyen ». Intolérant à l’inondation.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('4A', 'Grands arbres à semences et bois lourds. Plusieurs tolérants à la sécheresse.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('4B', 'Grande tolérance à la sécheresse, mais pas à l’ombre ou inondation.');
+INSERT INTO `inm5151_db`.`functional_group` (`group`, `description`) VALUES ('5', 'Espèces pionnières à très petites semences. Croissance rapide, tolérants à l’inondation, bois léger.');
 
 COMMIT;
 
@@ -178,10 +178,10 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `inm5151_db`;
-INSERT INTO `inm5151_db`.`family` (`id_family`, `name`) VALUES (0, 'Sapindaceae');
-INSERT INTO `inm5151_db`.`family` (`id_family`, `name`) VALUES (1, 'Juglandaceae');
-INSERT INTO `inm5151_db`.`family` (`id_family`, `name`) VALUES (2, 'Malvaceae');
-INSERT INTO `inm5151_db`.`family` (`id_family`, `name`) VALUES (3, 'Fagaceae');
+INSERT INTO `inm5151_db`.`family` (`name`) VALUES ('Sapindaceae');
+INSERT INTO `inm5151_db`.`family` (`name`) VALUES ('Juglandaceae');
+INSERT INTO `inm5151_db`.`family` (`name`) VALUES ('Malvaceae');
+INSERT INTO `inm5151_db`.`family` (`name`) VALUES ('Fagaceae');
 
 COMMIT;
 
@@ -191,9 +191,9 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `inm5151_db`;
-INSERT INTO `inm5151_db`.`genre` (`id_genre`, `name`) VALUES (0, 'Acer');
-INSERT INTO `inm5151_db`.`genre` (`id_genre`, `name`) VALUES (1, 'Catalpa');
-INSERT INTO `inm5151_db`.`genre` (`id_genre`, `name`) VALUES (2, 'Tilia');
+INSERT INTO `inm5151_db`.`genre` (`name`) VALUES ('Acer');
+INSERT INTO `inm5151_db`.`genre` (`name`) VALUES ('Catalpa');
+INSERT INTO `inm5151_db`.`genre` (`name`) VALUES ('Tilia');
 
 COMMIT;
 
@@ -203,10 +203,10 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `inm5151_db`;
-INSERT INTO `inm5151_db`.`location` (`id_location`, `latitude`, `longitude`) VALUES (0, '45.72587', '-73.45703');
-INSERT INTO `inm5151_db`.`location` (`id_location`, `latitude`, `longitude`) VALUES (1, '45.77804', '-73.40377');
-INSERT INTO `inm5151_db`.`location` (`id_location`, `latitude`, `longitude`) VALUES (2, '45.7259', '-73.45707');
-INSERT INTO `inm5151_db`.`location` (`id_location`, `latitude`, `longitude`) VALUES (3, '45.73708', '-73.44643');
+INSERT INTO `inm5151_db`.`location` (`latitude`, `longitude`) VALUES ('45.72587', '-73.45703');
+INSERT INTO `inm5151_db`.`location` (`latitude`, `longitude`) VALUES ('45.77804', '-73.40377');
+INSERT INTO `inm5151_db`.`location` (`latitude`, `longitude`) VALUES ('45.7259', '-73.45707');
+INSERT INTO `inm5151_db`.`location` (`latitude`, `longitude`) VALUES ('45.73708', '-73.44643');
 
 COMMIT;
 
@@ -216,9 +216,9 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `inm5151_db`;
-INSERT INTO `inm5151_db`.`tree` (`id_tree`, `date_plantation`, `date_measure`, `details_url`, `id_type`, `id_genre`, `id_family`, `id_functional_group`, `id_location`) VALUES (0, '2019-07-12', '2021-06-05', 'https://arbres.hydroquebec.com/fiche-arbre-arbuste/4857', 1, 0, 1, 2, 0);
-INSERT INTO `inm5151_db`.`tree` (`id_tree`, `date_plantation`, `date_measure`, `details_url`, `id_type`, `id_genre`, `id_family`, `id_functional_group`, `id_location`) VALUES (1, '2018-06-24', '2022-09-16', 'https://arbres.hydroquebec.com/fiche-arbre-arbuste/4552', 2, 2, 2, 5, 1);
-INSERT INTO `inm5151_db`.`tree` (`id_tree`, `date_plantation`, `date_measure`, `details_url`, `id_type`, `id_genre`, `id_family`, `id_functional_group`, `id_location`) VALUES (2, '2017-10-12', '2023-01-11', 'https://arbres.hydroquebec.com/fiche-arbre-arbuste/4705', 0, 3, 3, 4, 2);
+INSERT INTO `inm5151_db`.`tree` (`date_plantation`, `date_measure`, `details_url`, `id_type`, `id_genre`, `id_family`, `id_functional_group`, `id_location`) VALUES ('2019-07-12', '2021-06-05', 'https://arbres.hydroquebec.com/fiche-arbre-arbuste/4857', 1, 1, 1, 2, 2);
+INSERT INTO `inm5151_db`.`tree` (`date_plantation`, `date_measure`, `details_url`, `id_type`, `id_genre`, `id_family`, `id_functional_group`, `id_location`) VALUES ('2018-06-24', '2022-09-16', 'https://arbres.hydroquebec.com/fiche-arbre-arbuste/4552', 2, 2, 2, 5, 1);
+INSERT INTO `inm5151_db`.`tree` (`date_plantation`, `date_measure`, `details_url`, `id_type`, `id_genre`, `id_family`, `id_functional_group`, `id_location`) VALUES ('2017-10-12', '2023-01-11', 'https://arbres.hydroquebec.com/fiche-arbre-arbuste/4705', 2, 3, 3, 4, 3);
 
 COMMIT;
 
