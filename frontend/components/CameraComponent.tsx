8b6493ea67d591873575a8e83e79fb6d99f4a8c6
realDPS <ek791957@ens.uqam.ca>
2024-10-12 00:47:11 -0400

    update CameraComponent with Photo Review component

--- frontend/components/CameraComponent.tsx
+++ frontend/components/CameraComponent.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
 import { Camera, CameraType, CameraView } from "expo-camera";
+import PhotoReviewComponent from "./PhotoReviewComponent";
 
 interface CameraComponentProps {
   onCapture: (photo: any) => void;
@@ -10,6 +11,7 @@ const CameraComponent: React.FC<CameraComponentProps> = ({ onCapture }) => {
   const [isCameraReady, setIsCameraReady] = useState(false);
   const [facing, setFacing] = useState<CameraType>("back");
   const [cameraRef, setCameraRef] = useState<CameraView | null>();
+  const [photo, setPhoto] = useState<any>(null);
 
   const handleCameraReady = () => {
     setIsCameraReady(true);
@@ -18,31 +20,47 @@ const CameraComponent: React.FC<CameraComponentProps> = ({ onCapture }) => {
   const takePicture = async () => {
     if (cameraRef && cameraRef && isCameraReady) {
       const photo = await cameraRef.takePictureAsync();
-      onCapture(photo); // photo must be sent to the parent component via the onCapture prop.
+      setPhoto(photo);
     }
   };
+  
+  const onConfirm=()=>{
+    console.log("go to next page");
+    onCapture(photo); // photo must be sent to the parent component via the onCapture prop.
+    
+  }
+  const onDeny=()=>{
+    console.log("take another picture");
+    setPhoto(null);
+  }
 
   return (
     <View style={styles.container}>
-      <CameraView
-        style={styles.camera}
-        facing="back"
-        ref={(ref) => setCameraRef(ref)}
-        onCameraReady={handleCameraReady}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            padding: 20,
-          }}
+      {!photo ? ( // Conditionally render CameraView if photo is NULL
+        <CameraView
+          style={styles.camera}
+          facing="back"
+          ref={(ref) => setCameraRef(ref)}
+          onCameraReady={handleCameraReady}
         >
-          <Button
-            title="Take Picture"
-            onPress={takePicture}
-          />
-        </View>
-      </CameraView>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              padding: 20,
+            }}
+          >
+            <Button title="Take Picture" onPress={takePicture} />
+          </View>
+        </CameraView>
+      ) : (
+        // Conditionally render PhotoReviewComponent if a photo is NOT NULL
+        <PhotoReviewComponent
+          photo={photo}
+          onConfirm={onConfirm}
+          onDeny={onDeny}
+        />
+      )}
     </View>
   );
 };
