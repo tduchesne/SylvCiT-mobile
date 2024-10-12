edb47a0abedb97eca67ceab05ca3914e48e871c4
Christian Castaneda <castaneda_garcia.christian_jairo@courrier.uqam.ca>
2024-10-12 15:17:32 -0400

    Removing comments and unused imports

--- frontend/app/(tabs)/tree-analyzer/analysis-results.tsx
+++ frontend/app/(tabs)/tree-analyzer/analysis-results.tsx
@@ -1,5 +1,4 @@
-import { Image, StyleSheet, Platform, View } from "react-native";
-import Screen from "@/components/Screen";
+import { StyleSheet, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { useLocalSearchParams } from "expo-router";
 
@@ -8,7 +7,8 @@ export default function AnalyseResults() {
   const parsedPhoto = JSON.parse(photo as string); // Parse the photo back into an object
   console.log("analysis results...");
   console.log(parsedPhoto);
-  
+  // Add AI analysis here! [3. Intégrer API AI pour l'analyse de photo]
+
   return (
     <View>
       <ThemedText>AnalyseResults here!</ThemedText>
--- frontend/components/CameraComponent.tsx
+++ frontend/components/CameraComponent.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
-import { View, Button, Text, StyleSheet } from "react-native";
-import { Camera, CameraType, CameraView } from "expo-camera";
+import React, { useState } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import { CameraView } from "expo-camera";
 import PhotoReviewComponent from "./PhotoReviewComponent";
 
 interface CameraComponentProps {
@@ -9,7 +9,6 @@ interface CameraComponentProps {
 
 const CameraComponent: React.FC<CameraComponentProps> = ({ onCapture }) => {
   const [isCameraReady, setIsCameraReady] = useState(false);
-  const [facing, setFacing] = useState<CameraType>("back");
   const [cameraRef, setCameraRef] = useState<CameraView | null>();
   const [photo, setPhoto] = useState<any>(null);
 
@@ -23,19 +22,18 @@ const CameraComponent: React.FC<CameraComponentProps> = ({ onCapture }) => {
       setPhoto(photo);
     }
   };
-  
-  const onConfirm=()=>{
-    console.log("go to next page");
-    onCapture(photo); // photo must be sent to the parent component via the onCapture prop.
-  }
-  const onDeny=()=>{
-    console.log("take another picture");
+
+  const onConfirm = () => {
+    onCapture(photo);
+  };
+
+  const onDeny = () => {
     setPhoto(null);
-  }
+  };
 
   return (
     <View style={styles.container}>
-      {!photo ? ( // Conditionally render CameraView if photo is NULL
+      {!photo ? ( // if photo is NULL, show the camera
         <CameraView
           style={styles.camera}
           facing="back"
@@ -44,16 +42,19 @@ const CameraComponent: React.FC<CameraComponentProps> = ({ onCapture }) => {
         >
           <View
             style={{
-              position: 'absolute', // Position the button container absolutely
-              bottom: '5%', // Set distance from the bottom
-              left: '50%', // Center horizontally
+              position: "absolute",
+              bottom: "5%",
+              left: "50%",
             }}
           >
-            <Button title="PHOTO" onPress={takePicture} color="#056122"/>
+            <Button
+              title="PHOTO"
+              onPress={takePicture}
+              color="#056122"
+            />
           </View>
         </CameraView>
       ) : (
-        // Conditionally render PhotoReviewComponent if a photo is NOT NULL
         <PhotoReviewComponent
           photo={photo}
           onConfirm={onConfirm}
@@ -71,7 +72,6 @@ const styles = StyleSheet.create({
   camera: {
     flex: 1,
   },
-
 });
 
 export default CameraComponent;
