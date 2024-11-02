205112a479e8887676f1d7a5554299411446b736
Melissa Valesca Vallee <valesca@Melissas-MacBook-Air.local>
2024-11-02 09:23:49 -0400

    creer script pour ajouter utilisateurs

--- backend/models/user.py
+++ backend/models/user.py
@@ -3,6 +3,6 @@ from app import db
 class User(db.Model):
     __tablename__ = 'user' 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(255), nullable=False) 
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(128), nullable=False) 
     role = db.Column(db.Integer, nullable=False)  # Either 1 or 2
