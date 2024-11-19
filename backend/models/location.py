515fa9107af3182cef44fdcbb3e083d83c882c7f
Martin Phan <cc891259@ens.uqam.ca>
2024-11-19 14:10:45 -0500

    latitude et longitude db.Numeric

--- backend/models/location.py
+++ backend/models/location.py
@@ -4,8 +4,8 @@ class Location(db.Model):
     __tablename__ = 'location'
 
     id_location = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.String(45), nullable=False)
-    longitude = db.Column(db.String(45), nullable=False)
+    latitude = db.Column(db.Numeric(21,17), nullable=False)
+    longitude = db.Column(db.Numeric(21,17), nullable=False)
 
     tree = db.relationship('Tree', back_populates='location')
 
